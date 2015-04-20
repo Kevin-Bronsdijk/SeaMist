4c78bd862f7cdcc39a73fb39b92e5b564bea66f2
Kevin Bronsdijk <bronsdijk@bronsdijk.com>
2015-04-20 23:56:44 +0700

    Build script and Nuget pkg added

---
+++ build.ps1
@@ -0,0 +1,43 @@
+﻿
+$invocation = (Get-Variable MyInvocation).Value
+
+if($Invocation.MyCommand.Path)
+{
+    $baseDir = Split-Path $Invocation.MyCommand.Path
+}
+else
+{
+    $baseDir = $Invocation.InvocationName.Substring(0,$Invocation.InvocationName.LastIndexOf("\"));
+}
+
+$outputFolder = $baseDir + "\output"
+$releaseFolder = $baseDir + "\src\SeaMist\bin\Release"
+$msbuild = $env:systemroot + "\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe";
+$binDir = $baseDir + "\src\SeaMist\bin"
+$objDir = $baseDir + "\src\SeaMist\obj"
+
+If (Test-Path $outputFolder){
+	Remove-Item $outputFolder -Force -Recurse
+}
+
+# Build options
+$options = "/noconsolelogger /p:Configuration=Release /fl /flp:logfile=Build.log"
+
+# make sure our working directory is correct
+$baseDir = $baseDir + "\src"
+cd $baseDir 
+
+$clean = $msbuild + " ""SeaMist.sln"" " + $options + " /t:Clean"
+$build = $msbuild + " ""SeaMist.sln"" " + $options + " /t:Build"
+Invoke-Expression $clean
+Invoke-Expression $build
+
+[System.IO.Directory]::Move($releaseFolder, $outputFolder)
+
+If (Test-Path $binDir){
+	Remove-Item $binDir -Force -Recurse
+}
+If (Test-Path $objDir){
+	Remove-Item $objDir -Force -Recurse
+}
+
